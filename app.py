3564ebaaf4b711e2297cbf107c1f00017e0c5d38
Rodrigo Poblete <rorro.poblete@gmail.com>
2020-03-20 00:31:12 -0300

    Agrega template bootstrap y diseño del front

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -7,7 +7,7 @@ db = SQLAlchemy(app)
 
 @app.route('/')
 def home():
-    return "hello world"
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
